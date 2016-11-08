50a760147a2aa7ee2f8e9fbc39666173aa8acd18
JamesNikolaidis <dimitriosnikolaidis@hotmail.com>
2016-11-08 22:27:38 +0200

    update

---
+++ app/src/main/java/gr/edu/serres/TrancCoder_TheElucitated/Database/Database_Functions.java
@@ -0,0 +1,57 @@
+package gr.edu.serres.TrancCoder_TheElucitated.Database;
+
+import android.content.Context;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import gr.edu.serres.TrancCoder_TheElucitated.Objects.UsersObject;
+
+/**
+ * Created by James Nikolaidis on 11/8/2016.
+ */
+
+public class Database_Functions {
+
+    private Firebase mRoot,mUsers,mInventory,mItemLocation;
+    private FirebaseDatabase database;
+    private FirebaseStorage mStorage;
+    private static Database_Functions InstanceObject;
+
+
+    private Database_Functions(Context context) {
+
+        Firebase.setAndroidContext(context);
+        mRoot = new Firebase("https://the-elusidated-android-app.firebaseio.com/");
+        mUsers= new Firebase("https://the-elusidated-android-app.firebaseio.com/AppUsers");
+        mInventory = new Firebase("https://the-elusidated-android-app.firebaseio.com/Inventory");
+        mItemLocation = new Firebase("https://the-elusidated-android-app.firebaseio.com/Item Location");
+
+    }
+
+
+    public static Database_Functions getInstance(Context context){
+        if(InstanceObject==null){
+            InstanceObject = new Database_Functions(context);
+            return InstanceObject;
+        }
+        return InstanceObject;
+    }
+
+
+    public void SetUserInformation(UsersObject user){
+        String name,email,lastname,location;
+        mUsers.push().setValue(user);
+    }
+
+    public void SetInventory(Inventory inventory){
+        mInventory.push().setValue(inventory);
+    }
+
+
+
+
+
+
+}
