085923ab9f53dbe4ca3e3f43be0c7c5ede49ecd3
Alex <Alexandros_Chatzis@yahoo.com>
2016-11-17 20:41:30 +0200

    #37 Experience query

--- app/src/main/java/gr/edu/serres/TrancCoder_TheElucitated/Database/Database_Functions.java
+++ app/src/main/java/gr/edu/serres/TrancCoder_TheElucitated/Database/Database_Functions.java
@@ -2,9 +2,17 @@ package gr.edu.serres.TrancCoder_TheElucitated.Database;
 
 import android.content.Context;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import gr.edu.serres.TrancCoder_TheElucitated.Objects.InventoryClass;
 import gr.edu.serres.TrancCoder_TheElucitated.Objects.UsersObject;
@@ -56,6 +64,55 @@ public class Database_Functions {
         mInventory.push().setValue(inventory);
     }
 
+
+
+    
+    //START Of METHOD TO UPDATE USER EXPERIENCE
+    public void Change_User_Experience(final String Experience , String UserEmail  ){
+        //Query to find the Child with the given by user email
+        Query changeUserExperience  = mUsers.limitToFirst(1).orderByChild("email").equalTo(UserEmail);
+        //Triger query Child Listener
+
+
+        changeUserExperience.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ExpCounter = ExpCounter;
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+        changeUserExperience.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                //Gets Child Key
+                String key = dataSnapshot.getKey();
+
+                // Create new map for the new added object in the array
+                Map<String,Object> ExpMap = new HashMap<String,Object>();
+                //Gets UserObject Class object from JSON file
+                UsersObject usersObject = dataSnapshot.getValue(UsersObject.class);
+                ExpCounter = ExpCounter + Integer.valueOf(Experience);
+                //Put a new object to the Map with flag  name Experience and Value  equal to Exp+=experience
+                ExpMap.put("Experience",ExpCounter);
+                //Create a clone reference from JSON OBject find by the proper Key
+                Firebase cloneref = new Firebase("https://the-elusidated-android-app.firebaseio.com/AppUsers/"+key);
+                //Updates Child
+                cloneref.child("Experience").setValue(ExpCounter);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+
+
+        });
+
+    }//END OF EXPERIENCE METHOD
 
 
 
