30a67291597e03146d90a5229ed3933780499424
Alex <Alexandros_Chatzis@yahoo.com>
2016-11-17 20:53:15 +0200

    Update

--- app/src/main/java/gr/edu/serres/TrancCoder_TheElucitated/Objects/UsersObject.java
+++ app/src/main/java/gr/edu/serres/TrancCoder_TheElucitated/Objects/UsersObject.java
@@ -5,14 +5,21 @@ package gr.edu.serres.TrancCoder_TheElucitated.Objects;
  */
 
 public class UsersObject {
-    public String name, location, Experience, email;
 
-    public UsersObject(String... values) {
+    public String name,location,Experience,email;
+
+    public UsersObject(){}
+
+    public UsersObject(String ...values){
         this.name = values[0];
         this.location = values[1];
         this.Experience = values[2];
         this.email = values[3];
     }
 
+
+    public void SetUserEmail(String email){
+        this.email = email;
+    }
 
 }
