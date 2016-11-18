e84ba3d3665ba186eb29c5c3d5845dc22a617b8a
HiBye-Yo <mpampik@windowslive.com>
2016-11-18 14:09:55 +0200

    "Git test"

--- main/java/gr/edu/serres/TrancCoder_TheElucitated/MapsActivity.java
+++ main/java/gr/edu/serres/TrancCoder_TheElucitated/MapsActivity.java
@@ -1,24 +1,72 @@
 package gr.edu.serres.TrancCoder_TheElucitated;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+
+import gr.edu.serres.TrancCoder_TheElucitated.Services.BackgroundSoundService;
+
+enum MapOptions{SATELITE,TERRAIN,HYBRID,NORMAL;
+    public static int getOption(MapOptions mapOption){
+        switch (mapOption){
+            case SATELITE:
+                return GoogleMap.MAP_TYPE_SATELLITE;
+            case TERRAIN:
+                return GoogleMap.MAP_TYPE_TERRAIN;
+            case HYBRID:
+                return GoogleMap.MAP_TYPE_HYBRID;
+            case NORMAL:
+                return GoogleMap.MAP_TYPE_NORMAL;
+            default:
+                return GoogleMap.MAP_TYPE_NORMAL;
+        }
+    }
+};
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    Intent backgroundMusic;
+    Spinner pickItemSpinner;
     private GoogleMap mMap;
+    private Button mapOptionsButton;
+    MapOptions mapOption;
+    boolean mapReady = false;
+    Marker markerTest;
+    BitmapDescriptor markerIcon;
+    GroundOverlay staticIcon;
+    String itemSelected;
+    LatLng itemSelectedLocation;
+    static final LatLng TEI = new LatLng(41.075477, 23.553576);
+    float MapZoom = 16.5f;
 
+    Dummy inventoryUserItem;
+    //private HashMap<MapOptions,String> mapOptionsStringHashMap;
+    //GoogleMap.MAP_TYPE_SATELITE
     //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +74,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        inventoryUserItem = new Dummy();
+        inventoryUserItem.inventory.addItem(Dummy.Item.getItem("magnifier",41.069131,23.55389,R.mipmap.ic_launcher));
+        inventoryUserItem.inventory.addItem(Dummy.Item.getItem("handcuffs",41.07902,23.553690,R.mipmap.ic_launcher));
+        inventoryUserItem.inventory.addItem(Dummy.Item.getItem("glasses",41.07510,23.552997,R.mipmap.ic_launcher));
+
         mapFragment.getMapAsync(this);
+
+        mapOptionsButton = (Button) findViewById(R.id.map_options);
+        mapOption = MapOptions.NORMAL;
+        mapOptionsButton.setText(mapOption.toString());
+        mapOptionsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                switch(mapOption){
+                    case NORMAL:
+                        mapOption = MapOptions.TERRAIN;
+                        break;
+                    case TERRAIN:
+                        mapOption = MapOptions.SATELITE;
+                        break;
+                    case SATELITE:
+                        mapOption = MapOptions.HYBRID;
+                        break;
+                    case HYBRID:
+                        mapOption = MapOptions.NORMAL;
+                        break;
+                    default:
+                        mapOption = MapOptions.NORMAL;
+                        break;
+                }
+                mapOptionsButton.setText(mapOption.toString());
+                if(mapReady)mMap.setMapType(MapOptions.getOption(mapOption));
+            }
+        });
+        markerIcon = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher);
+
+
+        pickItemSpinner = (Spinner)findViewById(R.id.pick_item_spinnerr);
+        ArrayAdapter<String> pickItemAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,inventoryUserItem.inventory.getItemNames());
+        //pickItemSpinner.setBackgroundColor(Color.WHITE);//0xFFFFFFFF);
+        pickItemSpinner.setAdapter(pickItemAdapter);
+        pickItemSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //TODO
+                itemSelected = String.valueOf(adapterView.getItemAtPosition(i));
+                if(mapReady && !(inventoryUserItem.inventory.getItems().isEmpty())) {
+                    itemSelectedLocation = inventoryUserItem.inventory.getItemLocationByName(itemSelected);
+                    mMap.animateCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(itemSelectedLocation.latitude,itemSelectedLocation.longitude) ,MapZoom));
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        // return List<Item> inventoryUserItem.inventory.getItems();
     }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -76,7 +183,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ////////////////////////////////
 
 
-        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        //mMap.animateCamera(CameraUpdateFactory.zoomIn());
 
+        /*markerTest = mMap.addMarker(new MarkerOptions()
+                .position(TEI)
+                .title("Magnifier")
+                .icon(markerIcon));*/
+        double smallDistance = 0.0001;
+        staticIcon = mMap.addGroundOverlay(new GroundOverlayOptions()
+        .image(markerIcon)
+        .positionFromBounds(new LatLngBounds(TEI,new LatLng(TEI.latitude+smallDistance,TEI.longitude+smallDistance)))
+        );
+        mMap.animateCamera( CameraUpdateFactory.newLatLngZoom(TEI , MapZoom) );
+        mapReady = true;
+
+        inventoryUserItem.inventory.drawItems(mMap);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        backgroundMusic=new Intent(this, BackgroundSoundService.class);
+        startService(backgroundMusic);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onStop();
+        stopService(backgroundMusic);
     }
 }
