75cfa1b1b07ceac8a4b5e2443cff7b23096f5e5c
TasosTilsi <tasostilsi@hotmail.com>
2016-11-30 01:13:27 +0200

    Inventory Layout, Dialogs activity tomorrow again!

---
+++ app/src/main/java/gr/edu/serres/TrancCoder_TheElucitated/InventoryScreenActivity.java
@@ -0,0 +1,45 @@
+package gr.edu.serres.TrancCoder_TheElucitated;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import gr.edu.serres.TrancCoder_TheElucitated.Objects.InventoryClass;
+
+/**
+ * Created by tasos on 30/11/2016.
+ */
+
+public class InventoryScreenActivity extends AppCompatActivity {
+
+    private static int counter = 0;
+    private ArrayList<String> oneOne;
+    private ListView listView;
+    private InventoryClass inventory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.inventory_layout);
+
+
+        oneOne = new ArrayList<>();
+        //Toast.makeText(getApplicationContext(),String.valueOf(delay.length),Toast.LENGTH_SHORT).show();
+
+        while (counter < inventory.returnItemArray().size()) {
+            oneOne.add(inventory.returnItemArray().get(counter));
+            counter++;
+        }
+        counter = 0;
+
+        listView = (ListView) this.findViewById(R.id.ListView);
+
+        ListAdapter adapter = new CustomAdapter(getApplicationContext(), oneOne);
+        listView.setAdapter(adapter);
+
+    }
+}
